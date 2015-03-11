ef2ce9c1357f9d303467a5e3eb4a174692244d0d
Mastronardi, Frank <Frank.Mastronardi@macmillan.com>
2015-03-11 19:53:38 -0400

    new files

---
+++ mycoursexml.cpp
@@ -0,0 +1,5 @@
+#include "mycoursexml.h"
+
+myCourseXml::myCourseXml()
+{
+}
---
+++ mycoursexml.h
@@ -0,0 +1,10 @@
+#ifndef MYCOURSEXML_H
+#define MYCOURSEXML_H
+
+class myCourseXml
+{
+public:
+    myCourseXml();
+};
+
+#endif // MYCOURSEXML_H
